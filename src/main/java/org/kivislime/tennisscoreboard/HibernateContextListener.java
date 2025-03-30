5efd6d962b2f00c0fb623aa4530026341666029d
spinachh <kivislime@HypocritPc.>
2025-03-30 16:20:49 +0700

    refactor: remove redundant context attribute in HibernateContextListener

--- src/main/java/org/kivislime/tennisscoreboard/HibernateContextListener.java
+++ src/main/java/org/kivislime/tennisscoreboard/HibernateContextListener.java
@@ -5,9 +5,7 @@ import jakarta.servlet.ServletContextListener;
 
 public class HibernateContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
-        sce.getServletContext().setAttribute("hibernate.context", sce.getServletContext());
         HibernateUtil.getSessionFactory();
-
     }
     public void contextDestroyed(ServletContextEvent sce) {
         HibernateUtil.getSessionFactory().close();
