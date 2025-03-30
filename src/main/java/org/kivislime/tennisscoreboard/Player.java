e039e6d63a021e81f28ce8050d24c22bb7ccacad
spinachh <kivislime@HypocritPc.>
2025-03-30 16:07:23 +0700

    feat(entity): add Player entity with JPA mappings

---
+++ src/main/java/org/kivislime/tennisscoreboard/Player.java
@@ -0,0 +1,22 @@
+package org.kivislime.tennisscoreboard;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "players")
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 30)
+    private String name;
+}
