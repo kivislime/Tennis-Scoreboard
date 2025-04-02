da215f2f13094ca701e10169b81448f24bd4c3ba
spinachh <kivislime@HypocritPc.>
2025-04-02 16:30:49 +0700

    style: format

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,6 @@ INSERT INTO players(name)
 VALUES ('Johan'),
        ('Pohan'),
        ('Kohan');
-
 INSERT INTO matches(player1, player2, winner)
 VALUES (1, 2, 1),
-       (1, 3, 3)
+       (1, 3, 3);
