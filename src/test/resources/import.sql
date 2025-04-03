cffabe645da50a66c288b28c4cbf6d6afc619aba
spinachh <kivislime@HypocritPc.>
2025-04-03 14:22:48 +0700

    test: add repository, mockito and service tests

---
+++ src/test/resources/import.sql
@@ -0,0 +1,4 @@
+/* @formatter:off */
+INSERT INTO players(name) VALUES ('Johan'), ('Pohan'), ('Kohan');
+INSERT INTO matches(player1, player2, winner) VALUES (1, 2, 1), (1, 3, 3);
+/* @formatter:on */
