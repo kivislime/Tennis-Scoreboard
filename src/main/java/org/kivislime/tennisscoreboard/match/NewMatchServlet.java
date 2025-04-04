3db797cb4b20fba3f78b28c6ee832d70224ff63f
spinachh <kivislime@HypocritPc.>
2025-04-04 17:25:34 +0700

    feat: integrate live match creation with redirect

--- src/main/java/org/kivislime/tennisscoreboard/match/NewMatchServlet.java
+++ src/main/java/org/kivislime/tennisscoreboard/match/NewMatchServlet.java
@@ -1,13 +1,25 @@
 package org.kivislime.tennisscoreboard.match;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.UUID;
 
+@WebServlet("/new-match")
 public class NewMatchServlet extends HttpServlet {
+    private MatchService matchService;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext context = getServletContext();
+        matchService = (MatchService) context.getAttribute("matchService");
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
@@ -15,11 +27,24 @@ public class NewMatchServlet extends HttpServlet {
         String secondPlayerName = req.getParameter("second_player_name");
 
         //TODO: validate in util class
-        if (firstPlayerName == null || secondPlayerName == null) {
+        if (firstPlayerName == null || secondPlayerName == null ||
+                firstPlayerName.isBlank() || secondPlayerName.isBlank()) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
 
+        if (firstPlayerName.equals(secondPlayerName)) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
 
+        //TODO: Set status code in other controllers, when success and fail in error response
+        UUID matchId = matchService.createLiveMatchSession(firstPlayerName, secondPlayerName);
+        String matchIdString = matchId.toString();
+
+        resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + matchIdString);
+
+        //TODO: remove, or add logger
+        System.out.println("New match UUID: " + matchIdString);
     }
 }
