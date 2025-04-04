d0c3cc28b25b7b81bcbcb87960700543069e6f57
spinachh <kivislime@HypocritPc.>
2025-04-04 17:25:17 +0700

    feat: add MatchScoreServlet for live score API

---
+++ src/main/java/org/kivislime/tennisscoreboard/match/MatchScoreServlet.java
@@ -0,0 +1,58 @@
+package org.kivislime.tennisscoreboard.match;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.kivislime.tennisscoreboard.JsonUtil;
+
+import java.io.IOException;
+
+@WebServlet("/match-score")
+public class MatchScoreServlet extends HttpServlet {
+    private MatchService matchService;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext context = getServletContext();
+        matchService = (MatchService) context.getAttribute("matchService");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+
+        String matchId = req.getParameter("uuid");
+        if (matchId == null || matchId.isBlank()) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        MatchScoreDto matchScoreDto = matchService.getLiveMatchScore(matchId);
+        String matchScoreJson = JsonUtil.toJson(matchScoreDto);
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().write(matchScoreJson);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+
+        String matchId = req.getParameter("uuid");
+        String playerNumber = req.getParameter("player_number");
+
+        if (matchId == null || playerNumber == null || matchId.isBlank() || playerNumber.isBlank()) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        //TODO: if match end?
+        MatchScoreDto matchScoreDto = matchService.increaseMatchScore(matchId, playerNumber);
+        String matchScoreJson = JsonUtil.toJson(matchScoreDto);
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().write(matchScoreJson);
+    }
+}
