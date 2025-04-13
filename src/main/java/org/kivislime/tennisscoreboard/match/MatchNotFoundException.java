353df6f75e9990637f7402fa6b56cb79fa776f3f
spinachh <kivislime@HypocritPc.>
2025-04-13 12:07:14 +0700

    refactor: remove unused methods from exception classes

--- src/main/java/org/kivislime/tennisscoreboard/match/MatchNotFoundException.java
+++ src/main/java/org/kivislime/tennisscoreboard/match/MatchNotFoundException.java
@@ -4,8 +4,4 @@ public class MatchNotFoundException extends RuntimeException {
     public MatchNotFoundException(String message) {
         super(message);
     }
-
-    public MatchNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
--- src/main/java/org/kivislime/tennisscoreboard/match/MatchRepositoryException.java
+++ src/main/java/org/kivislime/tennisscoreboard/match/MatchRepositoryException.java
@@ -3,11 +3,6 @@ package org.kivislime.tennisscoreboard.match;
 import org.hibernate.HibernateException;
 
 public class MatchRepositoryException extends RuntimeException {
-
-    public MatchRepositoryException(String message) {
-        super(message);
-    }
-
     public MatchRepositoryException(String message, HibernateException e) {
         super(message, e);
     }
