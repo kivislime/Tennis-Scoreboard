e24f03f74cc3b814c42d7fe945bc22a861935009
spinachh <kivislime@HypocritPc.>
2025-04-14 16:06:42 +0700

    refactor: replace standard logger to slf4j

--- src/main/java/org/kivislime/tennisscoreboard/ExceptionHandlerFilter.java
+++ src/main/java/org/kivislime/tennisscoreboard/ExceptionHandlerFilter.java
@@ -2,6 +2,7 @@ package org.kivislime.tennisscoreboard;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.kivislime.tennisscoreboard.match.MatchRepositoryException;
 import org.kivislime.tennisscoreboard.match.MatchScoreException;
 import org.kivislime.tennisscoreboard.match.MatchesNotFoundException;
@@ -9,11 +10,9 @@ import org.kivislime.tennisscoreboard.match.MaxGamesExceededException;
 import org.kivislime.tennisscoreboard.player.PlayerRepositoryException;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+@Slf4j
 public class ExceptionHandlerFilter implements Filter {
-    private final Logger logger = Logger.getLogger(ExceptionHandlerFilter.class.getName());
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -21,12 +20,12 @@ public class ExceptionHandlerFilter implements Filter {
             filterChain.doFilter(servletRequest, servletResponse);
         } catch (MatchesNotFoundException e) {
             ErrorResponse errorResponse = new ErrorResponse("NOT_FOUND", "Matches not found");
-            logger.log(Level.INFO, "Match not found: {0}", e.getMessage());
+            log.info("Match not found: {}", e.getMessage());
             ServletUtil.sendJsonError((HttpServletResponse) servletResponse, HttpServletResponse.SC_NOT_FOUND, errorResponse);
 
         } catch (MatchScoreException e) {
             ErrorResponse errorResponse = new ErrorResponse("NOT_FOUND", "Match score not found");
-            logger.log(Level.INFO, "Match score not found: {0}", e.getMessage());
+            log.info("Match score not found: {}", e.getMessage());
             ServletUtil.sendJsonError((HttpServletResponse) servletResponse, HttpServletResponse.SC_NOT_FOUND, errorResponse);
 
         } catch (MaxGamesExceededException e) {
@@ -34,17 +33,17 @@ public class ExceptionHandlerFilter implements Filter {
                     "MAX_GAMES_EXCEEDED",
                     "The match has been terminated because the maximum number of games was reached"
             );
-            logger.log(Level.INFO, "Match forcibly terminated due to max games: {0}", e.getMessage());
+            log.info("Match forcibly terminated due to max games: {}", e.getMessage());
             ServletUtil.sendJsonError((HttpServletResponse) servletResponse, HttpServletResponse.SC_CONFLICT, errorResponse);
 
         } catch (MatchRepositoryException | PlayerRepositoryException e) {
             ErrorResponse errorResponse = new ErrorResponse("INTERNAL_SERVER_ERROR", "Internal Server Error");
-            logger.log(Level.WARNING, "Internal Server Error: {0}", e.getMessage());
+            log.warn("Internal Server Error: {}", e.getMessage(), e);
             ServletUtil.sendJsonError((HttpServletResponse) servletResponse, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, errorResponse);
 
         } catch (Exception e) {
             ErrorResponse errorResponse = new ErrorResponse("INTERNAL_SERVER_ERROR", "Internal Server Error");
-            logger.log(Level.WARNING, "Unknown Error: {0}", e.getMessage());
+            log.error("Unknown Error: {}", e.getMessage(), e);
             ServletUtil.sendJsonError((HttpServletResponse) servletResponse, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, errorResponse);
         }
     }
